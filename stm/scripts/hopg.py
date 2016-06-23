c7dc5422759a67fc5755cfc812a0225196840788
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-06-23 19:22:03 +0200

    Fix fitting stuff

--- stm/scripts/hopg.py
+++ stm/scripts/hopg.py
@@ -24,32 +24,44 @@ def plane_chisq(params, x, y, z):
 if __name__ == '__main__':
 
     metadata, data = read_nid('data/hopg.nid')
-    width = float(metadata['DataSet-Info']['image size'].replace(',', '.')[:-2])
+    width = metadata.getfloat('DataSet-0:1', 'dim0range') * 1e9
 
-    points = np.linspace(0, width, data[1].shape[0])
+    height_range = metadata.getfloat('DataSet-0:1', 'dim2range')
+    height = data['DataSet-0:1'].astype('float64') * height_range * 1e9
+
+    points = np.linspace(0, width, height.shape[0])
     x, y = np.meshgrid(points, points)
 
     res = minimize(
         plane_chisq,
-        [0.0001, 0.0001, -0.01],
-        args=(x.ravel(), y.ravel(), data[1].ravel())
+        [0.0, -0.25, height.mean()],
+        args=(x.ravel(), y.ravel(), height.ravel()),
+        # tol=1e-15,
     )
 
-    corrected = data[1] - plane(x, y, *res.x)
+    a, b, c = res.x
+    print(res.x)
+    alpha_x = np.arctan(a)
+    alpha_y = np.arctan(b)
+    print(alpha_x, alpha_y)
 
     coordinates = peak_local_max(
         rescale_intensity(corrected),
         min_distance=6,
     )
+    height = height - plane(x, y, *res.x)
 
-    coordinates = coordinates * width / corrected.shape[0]
+    x = x / np.cos(alpha_x)
+    y = y / np.cos(alpha_y)
+    print(1 / np.cos(alpha_x), 1 / np.cos(alpha_y))
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1, aspect=1)
-    plot = ax.pcolormesh(x, y, corrected, cmap='viridis')
-    ax.plot(coordinates[:, 1], coordinates[:, 0], '.', ms=3)
+    plot = ax.pcolormesh(x, y, height, cmap='viridis')
+    # ax.plot(coordinates[:, 1], coordinates[:, 0], '.', ms=3)
     fig.colorbar(plot, ax=ax)
-    ax.set_xlim(0, width)
-    ax.set_ylim(0, width)
+    # ax.set_xlim(0, width)
+    # ax.set_ylim(0, width)
+    plt.show()
 
     fig.savefig('build/plots/hopg.pdf')
