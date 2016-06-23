cfa01f79a5b1412fed8d6b813ec0b2e7aa58f8a2
Kai Bruegge <kai.bruegge@udo.edu>
2016-06-23 19:38:25 +0200

    find teh constant

--- stm/scripts/hopg.py
+++ stm/scripts/hopg.py
@@ -1,11 +1,29 @@
 from read_nid import read_nid
-from skimage import img_as_float
 from skimage.exposure import rescale_intensity
 from skimage.feature import peak_local_max
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython import embed
 
 from scipy.optimize import minimize
+
+def distances(p, coordinates):
+    return np.sqrt((p[0] - coordinates[:,0])**2 + (p[1] - coordinates[:,1])**2)
+
+
+def grid_constants(coordinates):
+    g_means = []
+    g_stds = []
+    for p in coordinates:
+        # print(p)
+        ds = distances(p, coordinates)
+        #take 5 best distances for graphene
+        g = ds[np.argsort(ds)[1:6]]
+        g_means.append(g.mean())
+        g_stds.append(g.std())
+
+    return np.array(g_means),  np.array(g_stds)
+
 
 
 def plane(x, y, a, b, c):
@@ -28,7 +46,7 @@ if __name__ == '__main__':
 
     points = np.linspace(0, width, data[1].shape[0])
     x, y = np.meshgrid(points, points)
-
+    # embed()
     res = minimize(
         plane_chisq,
         [0.0001, 0.0001, -0.01],
@@ -39,15 +57,20 @@ if __name__ == '__main__':
 
     coordinates = peak_local_max(
         rescale_intensity(corrected),
-        min_distance=6,
+        min_distance=7,
     )
+    print(coordinates)
 
     coordinates = coordinates * width / corrected.shape[0]
+    g, err = grid_constants(coordinates)
+    g_string = '\SI{{{:.4f} \pm {:.5f}}}{{\\nano\\meter}}'.format(g.mean(), err.mean())
+    with open('build/grid_constant.tex', 'w') as f:
+        f.write(g_string)
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1, aspect=1)
-    plot = ax.pcolormesh(x, y, corrected, cmap='viridis')
-    ax.plot(coordinates[:, 1], coordinates[:, 0], '.', ms=3)
+    plot = ax.pcolormesh(x, y, corrected, cmap='inferno')
+    ax.plot(coordinates[:, 1], coordinates[:, 0], '.', ms=2.5)
     fig.colorbar(plot, ax=ax)
     ax.set_xlim(0, width)
     ax.set_ylim(0, width)
